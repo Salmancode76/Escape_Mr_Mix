02169838e7c0bcc97187ba47a0653280835148ea
MahdiAllaith <mahdi-safa@hotmail.com>
2025-04-14 21:05:03 +0300

    To Get you started - Assets
    added alot off assets so group can start working.

---
+++ .history/Assets/Scenes/asd_20250414174022.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class asd : MonoBehaviour
+{
+
+    public Text My_asd;
+    // Start is called before the first frame update
+    public up(){
+        My_asd.text = "work asd";
+    }
+}
---
+++ .history/Assets/Scenes/asd_20250414175314.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void print(){
+        Debug.Log("asdDASFSOLDfnzlsdidhf bzkiudysfghali");
+    }
+}

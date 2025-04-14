84b7ac9139bab34682a0dba9fcd9a6380e20930a
MahdiAllaith <mahdi-safa@hotmail.com>
2025-04-14 07:08:43 +0300

    Added Main Menu
    when press new game it will take the player to my scene which is my level.

---
+++ .history/Assets/Game Systems/Sounds Manager/SoundFXManager_20250414050729.cs
@@ -0,0 +1,38 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+        }
+
+    }
+
+    public void playSoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume){
+            // spawn the gameObject
+            AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position , quaternion.identity);
+
+
+            // assign the audioClip
+            audioSource.clip = audioClip;
+
+            // assign volume
+            audioSource.volume = volume;
+
+            // play sound
+            audioSource.Play();
+
+            // get kength of sound FX clip
+            float clipLength = audioSource.clip.length;
+
+            // destroy the clip 
+            Destroy(audioSource.gameObject, clipLength);
+        }
+}
---
+++ .history/Assets/Game Systems/UI System/Main Menu/MainMenuHandler_20250414070011.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public void StartNewGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+}
